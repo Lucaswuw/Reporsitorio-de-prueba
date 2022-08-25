3015cce1c8815eecde42a1490c4f0d4a34f2c14a
Lucas Wu <lucaswu@200-87-219-eduroam-al.wlan.net.uc.cl>
2022-08-25 14:50:54 -0400

    Prueba 1

---
+++ prueba.py
@@ -0,0 +1,43 @@
+#Ejercicio Ayundatía 3
+
+########################## Parte 1: Leer un archivo ##########################
+
+# Lo primero que debes hacer es abrir el archivo y 
+# cargar los datos para utilizarlos posteriormente en el programa.
+# Para verificar si se cargaron bien los datos, imprime la primera línea del archivo.
+
+# Resultado esperado
+# Film;Opening Weekend;Worldwide Gross;Budget
+
+# Escribe tu código acá
+
+
+
+
+
+
+########################## Parte 2: Limpiar los datos ##########################
+
+# Luego de cargar los datos, debemos darles un formato que nos permita 
+# elaborar la visualización. Necesitamos que tengan un formato que:
+# - Permita ser leído por otros lenguajes de programación.
+# - Hagan sentido según su contexto
+
+# La base de datos que estamos utilizando contiene los siguientes errores (los que debes corregir):
+# - La columna Opening Weekend contiene puntos para hacer un separación tres cifras 
+# (como es un entero, no debería tener separaciones).
+# - La columna Worldwide Gross contiene comas (y algunos puntos) para hacer un 
+# separación tres cifras (como es un entero, no debería tener separaciones).
+# - La columna Budget contiene comas para separar decimales (como es un decimal, debería estar separado por un punto).
+
+# Te recomendamos crear una lista nueva donde se vayan guardando las líneas corregidas.
+
+# Resultado esperado
+# The To-Do List;1579402;3566225;1.5
+
+# Funciones recomendadas
+# split --> Separar una línea (string) en una lista
+# replace --> Reemplazar un caracter por otro dentro de un string
+# No olvides considerar los \n dentro de tu código
+
+# Escribe tu código acá
